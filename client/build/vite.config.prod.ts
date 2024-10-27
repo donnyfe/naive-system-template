98477f919e07060735ab5836dd764ab50057bbc3
donny <donnyfe@qq.com>
2024-10-27 23:59:50 +0800

    config: 修改配置

--- client/build/vite.config.prod.ts
+++ client/build/vite.config.prod.ts
@@ -1,38 +1,45 @@
 import { mergeConfig } from 'vite'
-import { useCdnPlugin, useCompressPlugin, useImageminPlugin, useRemoveConsolePlugin, useVisualizerPlugin } from './plugins'
+import { useImageminPlugin, useRemoveConsolePlugin, useVisualizerPlugin } from './plugins'
 import baseConfig from './vite.config.base'
 
-export default mergeConfig({
-	mode: 'prod',
-	base: './',
-	plugins: [
-		// 移除console
-		useRemoveConsolePlugin,
-		// 图片压缩
-		useImageminPlugin(),
-		// cdn
-		useCdnPlugin(),
-		// 打包压缩
-		useCompressPlugin('gzip'),
-		// 分析报告
-		useVisualizerPlugin()
-	],
-	build: {
-		target: 'esnext',
-		sourcemap: true,
-		reportCompressedSize: false, // 启用/禁用 gzip 压缩大小报告
-		chunkSizeWarningLimit: 2000,
-		rollupOptions: {
-			output: {
-				chunkFileNames: 'static/js/[name]-[hash].js',
-				entryFileNames: 'static/js/[name]-[hash].js',
-				assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-				manualChunks(id: string) {
-					if (id.includes('node_modules')) {
-						return id.toString().split('node_modules/')[1].split('/')[0].toString()
-					}
+export default mergeConfig(
+	{
+		mode: 'prod',
+		base: './',
+		plugins: [
+			// 移除console
+			useRemoveConsolePlugin,
+			// 图片压缩
+			useImageminPlugin(),
+			// 分析报告
+			useVisualizerPlugin()
+		],
+		build: {
+			target: 'esnext',
+			sourcemap: true,
+			reportCompressedSize: false, // 启用/禁用 gzip 压缩大小报告
+			chunkSizeWarningLimit: 2000,
+
+			rollupOptions: {
+				output: {
+					// 分包策略
+					manualChunks: {
+						// 将 Vue 相关库打包到 vue.js
+						'chunk-vue': ['vue', 'vue-router', 'pinia'],
+						// 将 naive-ui 单独打包
+						'chunk-naive': ['naive-ui'],
+						// 工具库打包到 utils.js
+						'chunk-utils': ['@vueuse/core', 'radash', 'axios', 'qs', 'dayjs', 'spark-md5']
+					},
+					// 用于从入口点创建的块的打包输出格式[name]表示文件名,[hash]表示该文件内容hash值
+					chunkFileNames: 'assets/js/[name]-[hash].js',
+					// 用于输出静态资源的命名，[ext]表示文件扩展名
+					assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+					// 拆分js到模块文件夹
+					entryFileNames: 'assets/js/[name]-[hash].js'
 				}
 			}
 		}
-	}
-}, baseConfig)
+	},
+	baseConfig
+)
