cf8c80ba00ad0b438abdc939286637ca279c5eea
donny <donnyfe@qq.com>
2024-11-11 16:17:59 +0800

    fix: 调整类型声明

--- client/src/il8n/index.ts
+++ client/src/il8n/index.ts
@@ -1,10 +1,11 @@
 import type { App } from 'vue'
-import { createI18n } from 'vue-i18n'
 import type { NDateLocale, NLocale } from 'naive-ui'
+import type { Lang } from '@/types/global'
 import { dateZhCN, zhCN as zhCNLocale } from 'naive-ui'
-import { local } from '@/utils'
+import { createI18n } from 'vue-i18n'
 import enUS from './locales/en_US.json'
 import zhCN from './locales/zh_CN.json'
+import { local } from '@/utils'
 
 const defaultLocale = 'enUS'
 
@@ -22,16 +23,13 @@ export function setupI18n(app: App) {
 	app.use(i18n)
 }
 
-export function setLocale(locale: App.lang) {
+export function setLocale(locale: Lang) {
 	i18n.global.locale.value = locale
 }
 
 export const $t = i18n.global.t
 
-export const naiveI18nOptions: Record<
-	App.lang,
-	{ locale: NLocale | null; dateLocale: NDateLocale | null }
-> = {
+export const naiveI18nOptions: Record<Lang, { locale: NLocale | null; dateLocale: NDateLocale | null }> = {
 	zhCN: {
 		locale: zhCNLocale,
 		dateLocale: dateZhCN
